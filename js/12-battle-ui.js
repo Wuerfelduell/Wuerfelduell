2f033c3ae47ccc051ad02259ad6ed68925a60ab6
Wuerfelduell <Wuerfelduell@gmail.com>
2026-08-24 10:51:21 +0200

    Add files via upload

--- js/12-battle-ui.js
+++ js/12-battle-ui.js
@@ -222,7 +222,7 @@
     el.querySelector(":scope > img.die-art-sprite")?.remove();
     stampArtFaces(el,designKey,value);
     const dieWidth=el.getBoundingClientRect().width;
-    if(dieWidth>0) el.style.setProperty("--die-half",`${Math.max(8,dieWidth*0.2)}px`);
+    if(dieWidth>0) el.style.setProperty("--die-half",`${Math.max(10,dieWidth*0.36)}px`);
     const rotation=DIE_3D_ROTATION[value]||DIE_3D_ROTATION[1];
     if(!el.classList.contains("rolling")){
       el.style.setProperty("--die-rx",rotation[0]);
